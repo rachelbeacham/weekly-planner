9682f02b2f389eaf0fdc75c6536382e33ce6acd0
Rachel Beacham <rachelbeacham17@gmail.com>
2021-02-12 23:26:13 +0000

    Picked font and accent color - added header. #1

--- src/planner.jsx
+++ src/planner.jsx
@@ -3,6 +3,9 @@ import React from 'react';
 export default class Planner extends React.Component {
   render() {
     return(
+      <header className="d-flex justify-content-center">
+        <h1 className="salmon">Weekly Planner</h1>
+      </header>
     )
   }
 }
