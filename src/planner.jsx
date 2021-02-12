e12616d411968612265e689ec0a887cdec9775c5
Rachel Beacham <rachelbeacham17@gmail.com>
2021-02-12 23:43:36 +0000

    Created add entry button. #1

--- src/planner.jsx
+++ src/planner.jsx
@@ -3,9 +3,16 @@ import React from 'react';
 export default class Planner extends React.Component {
   render() {
     return(
+      <>
       <header className="d-flex justify-content-center">
         <h1 className="mt-4 salmon">Weekly Planner</h1>
       </header>
+      <main>
+        <div className="d-flex justify-content-center">
+          <button>Add Entry</button>
+        </div>
+      </main>
+      </>
     )
   }
 }
