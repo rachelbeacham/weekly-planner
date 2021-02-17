4c47c5eb385e6f686a9b3a212af8b77111648bea
Rachel Beacham <rachelbeacham17@gmail.com>
2021-02-17 01:45:33 +0000

    Style modal to match wireframe. #1

--- src/components/addEntryModal.jsx
+++ src/components/addEntryModal.jsx
@@ -4,12 +4,15 @@ export default class AddEntry extends React.Component {
   render() {
     return (
       <div className="d-flex justify-content-center align-items-center min-vh-100">
-        <div className="border border-dark barder-2 py-3 px-4">
+        <div className="border border-dark barder-2 p-4">
           <h2 className="salmon text-center">Add Entry</h2>
           <form>
             <input type="date"/>
-            <input type="time"/>
-            <input type="textarea" placeholder="Description" rows="5"/>
+            <input type="time" className="mx-3"/>
+            <textarea className="w-100 my-3" rows="5" placeholder="Description"></textarea>
+            <div className="text-end">
+              <button type="submit" className="py-1 px-3 rounded salmon hover-shadow btn btn-outline-secondary">Submit</button>
+            </div>
           </form>
         </div>
       </div>
