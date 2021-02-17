abe43946db7f9327be560f5df69d5596577be1f5
Rachel Beacham <rachelbeacham17@gmail.com>
2021-02-17 01:57:34 +0000

    Add handleClick method for add entry button. #1

--- src/planner.jsx
+++ src/planner.jsx
@@ -2,20 +2,30 @@ import React from 'react';
 import AddEntry from './components/addEntryModal';
 
 export default class Planner extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      view: 'main'
+    };
+    this.handleClick = this.handleClick.bind(this)
+  }
+  handleClick() {
+    this.setState({
+      view: 'add entry'
+    })
+  }
   render() {
     return(
-      <AddEntry />
-    /*
     <>
     <header className="d-flex justify-content-center">
       <h1 className="mt-4 salmon">Weekly Planner</h1>
     </header>
     <main>
       <div className="d-flex justify-content-center">
-        <button className="py-1 px-3 rounded salmon hover-shadow btn btn-outline-secondary">Add Entry</button>
+        <button onClick={this.handleClick} className="py-1 px-3 rounded salmon hover-shadow btn btn-outline-secondary">Add Entry</button>
       </div>
     </main>
-    </> */
+    </>
     )
   }
 }
