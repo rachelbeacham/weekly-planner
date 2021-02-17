e2dbb746d191d3f02b374a17625da114fe62849f
Rachel Beacham <rachelbeacham17@gmail.com>
2021-02-17 02:26:01 +0000

    Working on closing modal with submit #1

--- src/components/addEntryModal.jsx
+++ src/components/addEntryModal.jsx
@@ -1,17 +1,30 @@
 import React from 'react';
 
 export default class AddEntry extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      view: 'add entry'
+    };
+    this.handleSubmit = this.handleSubmit.bind(this)
+  }
+  handleSubmit(e) {
+    e.preventDefault();
+
+  }
+
   render() {
+
     return (
       <div className="d-flex justify-content-center align-items-center">
         <div className="border border-dark barder-2 p-4">
           <h2 className="salmon text-center">Add Entry</h2>
-          <form>
+          <form onSubmit={this.handleSubmit}>
             <input type="date"/>
             <input type="time" className="mx-3"/>
             <textarea className="w-100 my-3" rows="5" placeholder="Description"></textarea>
             <div className="text-end">
-              <button type="submit" className="py-1 px-3 rounded salmon hover-shadow btn btn-outline-secondary">Submit</button>
+              <input type="submit" className="py-1 px-3 rounded salmon hover-shadow btn btn-outline-secondary" />
             </div>
           </form>
         </div>
