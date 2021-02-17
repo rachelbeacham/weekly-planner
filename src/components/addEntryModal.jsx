af4efd499abb773dc02edb4110e864b24302ae64
Rachel Beacham <rachelbeacham17@gmail.com>
2021-02-17 01:30:31 +0000

    Added elements for add entry modal. #1

--- src/components/addEntryModal.jsx
+++ src/components/addEntryModal.jsx
@@ -3,7 +3,16 @@ import React from 'react';
 export default class AddEntry extends React.Component {
   render() {
     return (
-
+      <div className="d-flex justify-content-center align-items-center min-vh-100">
+        <div className="border border-dark barder-2 py-3 px-4">
+          <h2 className="salmon text-center">Add Entry</h2>
+          <form>
+            <input type="date"/>
+            <input type="time"/>
+            <input type="textarea" placeholder="Description" rows="5"/>
+          </form>
+        </div>
+      </div>
     )
   }
 }
--- src/planner.jsx
+++ src/planner.jsx
@@ -1,18 +1,21 @@
 import React from 'react';
+import AddEntry from './components/addEntryModal';
 
 export default class Planner extends React.Component {
   render() {
     return(
-      <>
-      <header className="d-flex justify-content-center">
-        <h1 className="mt-4 salmon">Weekly Planner</h1>
-      </header>
-      <main>
-        <div className="d-flex justify-content-center">
-          <button className="py-1 px-3 rounded salmon hover-shadow btn btn-outline-secondary">Add Entry</button>
-        </div>
-      </main>
-      </>
+      <AddEntry />
+    /*
+    <>
+    <header className="d-flex justify-content-center">
+      <h1 className="mt-4 salmon">Weekly Planner</h1>
+    </header>
+    <main>
+      <div className="d-flex justify-content-center">
+        <button className="py-1 px-3 rounded salmon hover-shadow btn btn-outline-secondary">Add Entry</button>
+      </div>
+    </main>
+    </> */
     )
   }
 }
